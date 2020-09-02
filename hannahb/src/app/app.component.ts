fec0a582fa01dad1667de099d0c0ed5b339e1e68
Hannah Berrisford <hannah.berrisford@student.manchester.ac.uk>
2020-09-02 14:56:48 +0100

    custom claims testing

--- hannahb/src/app/app.component.ts
+++ hannahb/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, ViewChild, OnInit } from '@angular/core';
 
 import * as firebase from "firebase/app";
 import * as admin from 'firebase-admin';
-import "firebase/auth";
+
 import "firebase/firestore";
 
 
